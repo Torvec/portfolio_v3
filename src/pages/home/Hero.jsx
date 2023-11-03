fbf5a1ce6e036e7409b908d0c249018880fcc78b
Edward Von Schondorf <edvonschondorf@gmail.com>
2023-11-02 22:24:49 -0700

    Add Hero section to home

---
+++ src/pages/home/Hero.jsx
@@ -0,0 +1,9 @@
+function Hero() {
+    return (
+        <div className="min-h-screen bg-zinc-700 flex justify-center items-center">
+            <h1 className="text-white font-black text-8xl uppercase">Edward Vonschondorf</h1>
+        </div>
+    );
+}
+
+export default Hero;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,9 @@
+import Hero from "./Hero";
+
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <Hero />
     </div>
   );
 }
